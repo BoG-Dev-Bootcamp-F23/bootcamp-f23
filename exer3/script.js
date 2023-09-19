eaf69eef810ded6e3b3016befbc4c0df1f98a7ae
cjlin123 <108144107+cjlin123@users.noreply.github.com>
2023-09-19 16:52:12 -0400

    fixed bug with input selection list

--- exer3/script.js
+++ exer3/script.js
@@ -1,30 +1,69 @@
 const inputField = document.querySelector("#input-field");
+const inputButton = document.querySelector("#search-button")
+
 const inputSelection = document.querySelector("#options-container");
-const selectionButtons = [document.querySelector("#spaghetti"), 
-    document.querySelector("#cats"), document.querySelector("#plat")];
+const spaghetti = document.querySelector("#spaghetti")
+const cats = document.querySelector("#cats")
+const plat = document.querySelector("#plat")
 
 const ogInputValue = "Search...";
-let newInputValue = ogInputValue;
+let actualInputValue = inputField.value;
+let toBeInputValue = ogInputValue;
 
+const validInput = [spaghetti.textContent, cats.textContent, plat.textContent];
+
+//When user clicks input field display list of options. When user clicks an option that option fills the input with the option and turns the text black.
+//User can still type in input field. If typed input (non-case sensitive) does not match one of the options, it is left gray. If it is, the input turns black.
 inputField.addEventListener("focus",() => {
-    if (inputField.value === ogInputValue) {
+    if (toBeInputValue === ogInputValue) {
         inputField.value = "";
     } else {
-        inputField.value = newInputValue;
+        inputField.value = toBeInputValue;;
     }
     inputSelection.style.visibility = "visible";
 })
 
 inputField.addEventListener("input", () => {
-    newInputValue = inputField.value;
+    toBeInputValue = inputField.value;
+    if (toBeInputValue.toLowerCase() === spaghetti.textContent || toBeInputValue.toLowerCase() === cats.textContent || toBeInputValue.toLowerCase() === plat.textContent) {
+        inputField.style.color = "black";
+    } else {
+        inputField.style.color = "rgba(104, 102, 102, 0.661)";
+    }
 })
 
 inputField.addEventListener("blur", () => {
-    if (newInputValue === "") {
-        newInputValue = ogInputValue;
+    if (toBeInputValue === "") {
+        inputField.value = ogInputValue;
+        inputField.style.color = "rgba(104, 102, 102, 0.661)";
+    } else if (validInput.includes(toBeInputValue)) {
+        inputField.value = toBeInputValue;
     }
-    inputField.value = newInputValue;
     inputSelection.style.visibility = "hidden";
 })
 
+spaghetti.addEventListener("mouseenter", () => {
+    toBeInputValue = spaghetti.textContent;
+});
+spaghetti.addEventListener("mouseleave", () => {
+    toBeInputValue = "";
+    inputField.style.color = "black";
+});
 
+cats.addEventListener("mouseenter", () => {
+    toBeInputValue = cats.textContent;
+});
+cats.addEventListener("mouseleave", () => {
+    toBeInputValue = "";
+    inputField.style.color = "black";
+});
+
+plat.addEventListener("mouseenter", () => {
+    toBeInputValue = plat.textContent;
+});
+plat.addEventListener("mouseleave", () => {
+    toBeInputValue = "";
+    inputField.style.color = "black";
+});
+
+inputButton.addEventListener('click')
